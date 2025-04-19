7bcd3379a80735d75a80ab67b180f542f66771fc
arazthexd <ghanbarpour.araz@gmail.com>
2025-04-19 18:18:39 +0330

    fix synroute storage initiation

--- src/druglab/synthesis/storage.py
+++ src/druglab/synthesis/storage.py
@@ -17,29 +17,40 @@ class SynRouteStorage(BaseStorage):
                  feats: np.ndarray = None,
                  fnames: List[str] = None,
                  featurizers: List[BaseFeaturizer | dict] = None):
-        super().__init__(routes, 
-                         fdtype=fdtype, 
-                         feats=feats, 
-                         fnames=fnames, 
-                         featurizers=featurizers)
         
+        routes_ = []
+
         self.pstore: MolStorage = MolStorage()
         self.rstore: MolStorage = MolStorage()
         self.rxnstore: RxnStorage = RxnStorage()
 
-        for route in self.routes:
-            
+        for route in routes:
+            route_ = SynthesisRoute()
+            route_.start()
             for mem in route.seq:
                 if mem.type == ActionTypes.REACTION:
-                    mem.idx += len(self.rxnstore)
+                    route_.add_reaction(route.reactions[mem.idx])
+                    route_.seq[-1].idx += len(self.rxnstore)
                 elif mem.type == ActionTypes.REACTANT:
-                    mem.idx += len(self.rstore)
-                elif mem.type in [ActionTypes.PRODUCT, ActionTypes.USEPROD]:
-                    mem.idx += len(self.pstore)
+                    route_.add_reactant(route.reactants[mem.idx])
+                    route_.seq[-1].idx += len(self.rstore)
+                elif mem.type == ActionTypes.PRODUCT:
+                    route_.add_product(route.products[mem.idx])
+                    route_.seq[-1].idx += len(self.pstore)
+                elif mem.type == ActionTypes.USEPROD:
+                    route_.use_product()
+            route_.end()
+            routes_.append(route_)
 
             self.pstore.extend(MolStorage(route.products))
             self.rstore.extend(MolStorage(route.reactants))
             self.rxnstore.extend(RxnStorage(route.reactions))
+
+        super().__init__(routes_, 
+                         fdtype=fdtype, 
+                         feats=feats, 
+                         fnames=fnames, 
+                         featurizers=featurizers)
 
     def featurize(self, 
                   featurizer: SynRouteFeaturizer, 
